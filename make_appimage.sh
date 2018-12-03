5eecbc225f80ac7fdc28c411cab3d8c4903f8054
Dominik Deak <deak.software@gmail.com>
2018-12-03 12:06:01 +1100

    Bugfix for script.

--- make_appimage.sh
+++ make_appimage.sh
@@ -2,8 +2,8 @@
 
 mkdir Build; cd Build
 
-wget -c https://raw.githubusercontent.com/AppImage/AppImages/master/pkg2appimage | exit 1
+wget -c https://raw.githubusercontent.com/AppImage/AppImages/master/pkg2appimage || exit 1
 
-chmod 766 pkg2appimage | exit 1
+chmod 766 pkg2appimage || exit 1
 
 ./pkg2appimage ../Recipe/OpenPHT.yml
